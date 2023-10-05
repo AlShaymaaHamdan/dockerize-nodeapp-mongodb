86276080a34486882632ba10fdce3563fb7f4bd2
AlShaymaaHamdan <101719699+AlShaymaaHamdan@users.noreply.github.com>
2023-10-05 16:13:15 +0300

    Update null.tf

--- Terraform/null.tf
+++ Terraform/null.tf
@@ -6,10 +6,15 @@ resource "null_resource" "runtime" {
   provisioner "local-exec" {
     command = <<EOF
       chmod +x ../Scripts/inventory.sh && chmod +x ../Scripts/config.sh
-  
+    EOF
+  }
+
+  provisioner "local-exec" {
+    command = <<EOF
       ../Scripts/config.sh ${aws_instance.tf-ec2.public_ip}
     EOF
   }
+
   # provisioner "local-exec" {
   #   command = <<EOF
   #       ansible-playbook -i ../Ansible/inventory ../Ansible/config-EC2.yml
