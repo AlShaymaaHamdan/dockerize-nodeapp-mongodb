750d3b1472f7fd743e93d69deba884a4e1fe293c
AlShaymaaHamdan <101719699+AlShaymaaHamdan@users.noreply.github.com>
2023-10-10 13:38:22 +0300

    Update config.sh

--- Scripts/config.sh
+++ Scripts/config.sh
@@ -2,6 +2,6 @@ cat <<EOF > ~/.ssh/config
 host remoteMachine
    HostName $1
    User ubuntu
-   identityFile $HOME/Terraform/tfproject.pem
+   identityFile /root/project/Terraform/tfproject.pem
    StrictHostKeyChecking=no
 EOF
